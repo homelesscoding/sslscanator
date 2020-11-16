4d6fec4331480ffc634381e18ecccbb967afa4f7
Matt <46228173+matthernet@users.noreply.github.com>
2020-11-16 21:14:44 +0100

    added improvements from version v 0.3.0

--- sslscanator.sh
+++ sslscanator.sh
@@ -1,40 +1,106 @@
 #!/bin/bash
 
-for i in `cat $1`;do
-        sslscan --no-color --no-failed --no-heartbleed --no-compression --no-renegotiation --no-fallback $i > ${i/:/_}
+# SSLscanator v0.3.0
+
+OPTARG=" "
+
+while getopts "t:l:" arg; do
+	case $arg in
+	t) 
+		target=`echo $OPTARG | cut -d ":" -f 1`
+		mkdir $target-temp
+		sslscan --no-color --no-failed --no-heartbleed --no-compression --no-renegotiation --no-fallback $OPTARG > $PWD/$target-temp/${OPTARG/:/_}
+		;;
+	l)
+		target=$OPTARG
+		mkdir $target-temp
+		for i in `cat $OPTARG`;do
+			sslscan --no-color --no-failed --no-heartbleed --no-compression --no-renegotiation --no-fallback $i > $PWD/$target-temp/${i/:/_}
+		done
+		;;
+	esac
 done
+temp=$PWD/$target-temp
+#-------------------------------------------------#
+grep -il --no-filename "SSLv2     enabled" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------SSLv2------" >> $PWD/$target-report
+	grep -il --no-filename "SSLv2     enabled" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il "SSLv3     enabled" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------SSLv3------" >> $PWD/$target-report
+	grep -il "SSLv3     enabled" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il "TLSv1.0" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------TLSv1.0------" >> $PWD/$target-report
+	grep -il "TLSv1.0" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il "CBC3" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------SWEET32------" >> $PWD/$target-report
+	grep -il "CBC3" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il "RC4" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------RC4------" >> $PWD/$target-report
+	grep -il "RC4" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il "SSLv3     enabled" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------POODLE------" >> $target-report
+	grep -il "SSLv3" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il -e " 56 bits" -e " 40 bits" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------Short key length of cipher suites enabled (Less that 128 bits)------" >> $PWD/$target-report
+	grep -il -e " 56 bits" -e " 40 bits" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
+grep -il -e " 56 bits" -e " 40 bits" $temp/* >> /dev/null
+if [ $? -eq 0 ]; then
+	echo "------Short key lenght of DHE cipher suites (Less than 2048 bits)------" >> $PWD/$target-report
+	grep -il "RSA Key Strength:    1024" $temp/* | rev | cut -d "/" -f 1 | rev >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+	echo " " >> $PWD/$target-report
+else
+	echo " "  >> /dev/null
+fi
+#-------------------------------------------------#
 
-echo "------SSLv2------" >> ${1}-report
-grep -il "SSLv2" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------SSLv3------" >> ${1}-report
-grep -il "SSLv3" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------TLSv1.0------" >> ${1}-report
-grep -il "TLSv1.0" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------SWEET32------" >> ${1}-report
-grep -il "CBC3" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------RC4------" >> ${1}-report
-grep -il "RC4" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------POODLE------" >> ${1}-report
-grep -il "SSLv3" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------Short key length of cipher suites enabled (Less that 128 bits)------" >> ${1}-report
-grep -il -e " 56 bits" -e " 40 bits" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-echo "------Short key lenght of DHE cipher suites (Less than 2048 bits)------" >> ${1}-report
-grep -il "RSA Key Strength:    1024" --exclude={sslscanator.sh,${1}-report} * >> ${1}-report
-echo " " >> ${1}-report
-echo " " >> ${1}-report
-
-find . -maxdepth 1 ! -name sslscanator.sh ! -name $1 ! -name ${1}-report -type f -delete
+rm -fr $temp
